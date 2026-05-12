43978ee2f9b700f6db40a8c6d43a247e99d6336b
fatal-zxc <faaatal_l@proton.me>
2026-05-12 20:34:45 +0300

    fix: correct types in nullToUndefined

--- lib/utils/null-to-undefined.util.ts
+++ lib/utils/null-to-undefined.util.ts
@@ -1,2 +1,6 @@
-export const nullToUndefined = <T extends object>(obj: T) =>
-	Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, v ?? undefined])) as T
+type NullToUndefined<T> = {
+	[K in keyof T]: null extends T[K] ? Exclude<T[K], null> | undefined : T[K]
+}
+
+export const nullToUndefined = <T extends object>(obj: T): NullToUndefined<T> =>
+	Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, v ?? undefined])) as NullToUndefined<T>
