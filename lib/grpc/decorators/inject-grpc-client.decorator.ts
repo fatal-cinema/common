ccbf11a3dc7e8bd50a08aba625d04d9a08404f98
fatal-zxc <faaatal_l@proton.me>
2026-05-20 22:23:18 +0300

    feat: types in InjectGrpcClient decorator

--- lib/grpc/decorators/inject-grpc-client.decorator.ts
+++ lib/grpc/decorators/inject-grpc-client.decorator.ts
@@ -1,5 +1,6 @@
 import { Inject } from '@nestjs/common'
 
 import { GRPC_CLIENT_PREFIX } from '../constants'
+import { GrpcPackageKey } from '../types'
 
-export const InjectGrpcClient = (name: string) => Inject(`${GRPC_CLIENT_PREFIX}_${name}`)
+export const InjectGrpcClient = (name: GrpcPackageKey) => Inject(`${GRPC_CLIENT_PREFIX}_${name}`)
